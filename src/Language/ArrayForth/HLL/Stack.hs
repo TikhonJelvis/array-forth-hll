e746a06012c207cf52538a2654d12e22739c2ad7
Tikhon Jelvis <tikhon@jelv.is>
2013-01-05 18:05:12 -0800

    Added a lightweight stack model.

---
+++ src/Language/ArrayForth/HLL/Stack.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE NamedFieldPuns #-}
+module Language.ArrayForth.Stack where
+
+data Stack a = Stack { dataS, retS :: [a]
+                     , a :: a }
+
+data Operation = Dup | Over | Drop | Pop | Push | ToA | FromA deriving (Show, Eq, Bounded, Enum)
+
+operate :: Stack a -> Operation -> Stack a
+operate stack@Stack {dataS = d:ds, retS = r:rs, a} opr = case opr of
+  Dup   -> stack { dataS = d:d:init ds }
+  Drop  -> stack { dataS = ds }
+  Push  -> stack { dataS = ds, retS = d:r:rs }
+  Pop   -> stack { dataS = r:d:ds, retS = rs }
+  ToA   -> stack { dataS = ds, a = d }
+  FromA -> stack { dataS = a:d:ds }
