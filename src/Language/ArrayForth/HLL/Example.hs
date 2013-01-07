3cddc214a79a68e45f6733a2f284bfcb46ef95cf
Tikhon Jelvis <tikhon@jelv.is>
2013-01-07 00:56:20 -0800

    Added more examples.

--- src/Language/ArrayForth/HLL/Example.hs
+++ src/Language/ArrayForth/HLL/Example.hs
@@ -17,17 +17,35 @@ import           Language.ArrayForth.HLL.Compile
 compiledToNative :: (Program, [F18Word]) -> NativeProgram
 compiledToNative (program, memory) = fmap Constant memory ++ toNative program
 
+-- You can look at any AST as a native program using `asNative ast'
+-- You can also use `asBits ast'
+
+-- A simple example program. This shows how to use variables,
+-- arithmetic and if statements.
 test :: AST
 test = do "a" =: 1
           "b" =: 2
-          if "a" > "b" then "c" =: 3 else "c" =: 4
+          "c" =: "a" + "b"
+          if "c" == 3 then "d" =: 3 else "d" =: 4
+
+-- We can also have arrays and array indexing with (!). However, no
+-- loops quite yet.
+test' :: AST
+test' = do "arr" `array` [1..5] -- declare an array with value [1,2,3,4,5]
+           "arr" ! 3            -- get the fourth element of the array
+           "arr" ! 3 =: 10      -- set the fourth element of the array to 10
 
 -- You can add language "constructs" pretty easily.
 when :: AST -> AST -> AST
 when cond expr = if cond then expr else nil
 
-asNative :: NativeProgram
-asNative = compiledToNative $ compile test
+-- This is how you would use the new "when" construct:
+testWhen :: AST
+testWhen = do "a" =: 10
+              when ("a" > 5) $ "a" =: (- "a")
 
-asBits :: [F18Word]
-asBits = fmap toBits asNative
+asNative :: AST -> NativeProgram
+asNative = compiledToNative . compile 
+
+asBits :: AST -> [F18Word]
+asBits = fmap toBits . asNative
